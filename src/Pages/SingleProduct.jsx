ab5aca39ee6c1e7f18300031c9b6889bd78adece
deshmukh612 <99964682+deshmukh612@users.noreply.github.com>
2022-07-20 22:18:27 +0530

    Single Produc Details pages

---
+++ src/Pages/SingleProduct.jsx
@@ -0,0 +1,221 @@
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Image,
+  Flex,
+  VStack,
+  Button,
+  Heading,
+  SimpleGrid,
+  StackDivider,
+  useColorModeValue,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useParams } from "react-router-dom";
+import { addProductToCart, getProduct } from "../Redux/action";
+
+export default function SingleProduct() {
+  const dispatch = useDispatch();
+  const id = 1;
+  //   const {id} = useParams()
+  const [currentProduct, setCurrentProduct] = useState({});
+  const chicken = useSelector((state) => state.reducer.chicken);
+
+  useEffect(() => {
+    if (chicken.length === 0) {
+      dispatch(getProduct());
+    }
+  }, [chicken?.length, dispatch]);
+
+  useEffect(() => {
+    if (id) {
+      const temp = chicken.find((item) => item.id === Number(id));
+      //   console.log(temp)
+      temp && setCurrentProduct(temp);
+    }
+  }, [chicken, id]);
+
+  const addToCartHandler = () => {
+    currentProduct && dispatch(addProductToCart(currentProduct));
+  };
+  console.log(chicken);
+  console.log(currentProduct);
+
+  return (
+    <Container maxW={"7xl"}>
+      <SimpleGrid
+        columns={{ base: 1, lg: 2 }}
+        spacing={{ base: 8, md: 10 }}
+        py={{ base: 18, md: 24 }}
+      >
+        <Flex>
+          <Image
+            rounded={"md"}
+            alt={"product image"}
+            src={currentProduct.image}
+            fit={"contain"}
+            align={"center"}
+            w={"100%"}
+            h={{ base: "100%", sm: "400px", lg: "500px" }}
+          />
+        </Flex>
+        <Stack spacing={{ base: 6, md: 10 }}>
+          <Box as={"header"}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={500}
+              fontSize={{ base: "2xl", sm: "4xl", lg: "2xl" }}
+            >
+              {currentProduct.title}
+            </Heading>
+            <Text
+              color={useColorModeValue("gray.900", "gray.400")}
+              fontWeight={300}
+              fontSize={"2xl"}
+            >
+              {currentProduct.price}
+            </Text>
+          </Box>
+
+          <Stack
+            spacing={{ base: 4, sm: 6 }}
+            direction={"column"}
+            divider={
+              <StackDivider
+                borderColor={useColorModeValue("gray.200", "gray.600")}
+              />
+            }
+          >
+            <VStack spacing={{ base: 4, sm: 6 }}>
+              <Text
+                color={useColorModeValue("gray.500", "gray.400")}
+                fontSize={"2xl"}
+                fontWeight={"300"}
+              >
+                {currentProduct.description}
+              </Text>
+              <Text fontSize={"md"} color={useColorModeValue("gray.500", "gray.400")}>
+                Licious Chicken Curry Cut is a mix of bone-in, small pieces of
+                breast, wing (without the tip), leg and thigh meat, enough to
+                feed a small to medium-sized family. This Small pack consists of
+                13-16 pieces of smaller cuts of chicken, for when you are making
+                a smaller portion of curry or looking to reduce cooking time.
+                Smaller pieces of chicken cook faster because of the higher
+                surface-to-volume ratio.
+              </Text>
+            </VStack>
+            <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={useColorModeValue("yellow.500", "yellow.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}
+              >
+                Features
+              </Text>
+
+              <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                <List spacing={2}>
+                  <ListItem>Chronograph</ListItem>
+                  <ListItem>Master Chronometer Certified</ListItem>{" "}
+                  <ListItem>Tachymeter</ListItem>
+                </List>
+                <List spacing={2}>
+                  <ListItem>Anti‑magnetic</ListItem>
+                  <ListItem>Chronometer</ListItem>
+                  <ListItem>Small seconds</ListItem>
+                </List>
+              </SimpleGrid>
+            </Box>
+            {/* <Box>
+              <Text
+                fontSize={{ base: "16px", lg: "18px" }}
+                color={useColorModeValue("yellow.500", "yellow.300")}
+                fontWeight={"500"}
+                textTransform={"uppercase"}
+                mb={"4"}
+              >
+                Product Details
+              </Text>
+
+              <List spacing={2}>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Between lugs:
+                  </Text>{" "}
+                  20 mm
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Bracelet:
+                  </Text>{" "}
+                  leather strap
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Case:
+                  </Text>{" "}
+                  Steel
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Case diameter:
+                  </Text>{" "}
+                  42 mm
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Dial color:
+                  </Text>{" "}
+                  Black
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Crystal:
+                  </Text>{" "}
+                  Domed, scratch‑resistant sapphire crystal with anti‑reflective
+                  treatment inside
+                </ListItem>
+                <ListItem>
+                  <Text as={"span"} fontWeight={"bold"}>
+                    Water resistance:
+                  </Text>{" "}
+                  5 bar (50 metres / 167 feet){" "}
+                </ListItem>
+              </List>
+            </Box> */}
+          </Stack>
+
+          <Button
+            rounded={"none"}
+            w={"full"}
+            mt={8}
+            size={"lg"}
+            py={"7"}
+            bg={useColorModeValue("gray.900", "gray.50")}
+            color={useColorModeValue("white", "gray.900")}
+            textTransform={"uppercase"}
+            _hover={{
+              transform: "translateY(2px)",
+              boxShadow: "lg",
+            }}
+            onClick={addToCartHandler}
+          >
+            Add to cart
+          </Button>
+
+          <Stack direction="row" alignItems="center" justifyContent={"center"}>
+            {/* <MdLocalShipping /> */}
+            <Text>2-3 business days delivery</Text>
+          </Stack>
+        </Stack>
+      </SimpleGrid>
+    </Container>
+  );
+}
