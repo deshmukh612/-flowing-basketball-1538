940790dddb1cc8655c1a492b1462d2866f99d572
deshmukh612 <99964682+deshmukh612@users.noreply.github.com>
2022-07-21 20:52:48 +0530

    cart page drawer and cart counter Page

--- src/Pages/MainRoutes.jsx
+++ src/Pages/MainRoutes.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Navbar from '../Components/Navbar'
-import Demo from './Demo'
-import Home from './Home'
-import SBCIndevidualPage from './SBCIndevidualPage'
-import Products from "../Components/Products"
-import SeaAndFish from "../Components/SeaAndFish"
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import Demo from "./Demo";
+import Home from "./Home";
+import SBCIndevidualPage from "./SBCIndevidualPage";
+import Products from "../Components/Products";
+import SeaAndFish from "../Components/SeaAndFish";
+import SingleProduct from "./SingleProduct";
 const MainRoutes = () => {
-  return (<>
-    <Navbar/>
-     <Routes>
-
-        <Route  path='/' element= {<Home/>} />
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
         {/* <Route path='/sbArray/:id' element = {<SBCIndevidualPage/>}/> */}
-        <Route  path='/abc/:id' element= {<Demo />}/>
-        <Route  path='/products' element= {<Products />}/>
-        <Route  path='/seaAndfish' element= {<SeaAndFish />}/>
-     </Routes>
-     </>
+        <Route path="/abc/:id" element={<Demo />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/seaAndfish" element={<SeaAndFish />} />
+      </Routes>
+    </>
+  );
+};
 
-  )
-}
-
-export default MainRoutes
+export default MainRoutes;
--- src/Redux/ProductReducer/reducer.js
+++ src/Redux/ProductReducer/reducer.js
@@ -1,41 +1,91 @@
-import * as types from "./actionTypes"
+import * as types from "./actionTypes";
 const initialState = {
- chicken: [],
- fish:[],
+  chicken: [],
+  fish: [],
   isLoading: false,
   isError: false,
+  cart: [],
 };
 
-export const reducer = (state = initialState,action) => {
-  const {type,payload} =action
+export const reducer = (state = initialState, action) => {
+  const { type, payload } = action;
 
-  switch(type){
+  switch (type) {
+    case types.GET_SHOES_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
 
-      case types.GET_SHOES_REQUEST :
-        return {
-          ...state,
-          isLoading :true,
-          isError:false
-        }
-       
-      case types.GET_SHOES_SUCCESS :
-          return {
-              ...state,
-              chicken:payload,
-               fish:payload,
-              isLoading :false,
-              isError:false
-          }
-          
-   
-      case types.GET_SHOES_FAILURE :
-          return {
-            ...state,
-              isLoading :false,
-              isError:true
-          }
+    case types.GET_SHOES_SUCCESS:
+      return {
+        ...state,
+        chicken: payload,
+        fish: payload,
+        isLoading: false,
+        isError: false,
+      };
 
-          
-          default : return state
+    case types.GET_SHOES_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case types.ADD_PRODUCT_CART_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case types.ADD_PRODUCT_CART_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        cart: [...state.cart, payload],
+      };
+    case types.ADD_PRODUCT_CART_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case types.FETCH_CART_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case types.FETCH_CART_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        cart: [...payload],
+      };
+    case types.FETCH_CART_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case types.REMOVE_PRODUCT_CART_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case types.REMOVE_PRODUCT_CART_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    default:
+      return state;
+  }
 };
-}
+
+export { reducer };
